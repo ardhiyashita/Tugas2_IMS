c3481f0f76fc300c54fc0621d0e983366a73c9f1
John Doe <ardhiyashita2002@gmail.com>
2022-03-13 00:27:19 +0800

    updated crud

---
+++ create.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="newStyle.css">
+    <link rel="stylesheet" href="projekStyle.css">
+    <title>create-movie</title>
+</head>
+<body>
+<!-- <div class="dark"> -->
+<div class="background">
+<div class="container">
+    <section class="py-5">
+        <div class="box-movie">
+            <div class="product-name mb-2" style="color: #A71C1C; font-weight: bold; text-align:center">Create Movie</div>
+            <div class="white-line mb-3"></div>
+                <div class="box-movie-info">
+                    <div class="container">
+                        <img class="img-movie mb-3" src="img/markus-spiske-BpgBohGAXS8-unsplash.jpg" alt="">
+                        <input id="foto" name="foto" type="file" class="form-control" style="width: 400px;">
+                    </div>
+                    <div class="box-empat" style="text-align: left; padding: 10px; box-shadow:none;">
+
+                        <div class="form-group mb-3">
+                            <label for="exampleFormControlSelect1" class="roboto-merah">Name of Movie</label>
+                                <input type="text" class="form-control ps-0 form-control-line" placeholder="   ex: Avatar:The Legend of Aang">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="exampleFormControlSelect1" class="roboto-merah">Release Date</label>  
+                                <input type="date" class="form-control ps-0 form-control-line" placeholder="   ex: 22 January 2021">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="exampleFormControlSelect1" class="roboto-merah">Rating</label>
+                                <input type="number" class="form-control ps-0 form-control-line" placeholder="   ex: 9">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="exampleFormControlSelect1" class="roboto-merah">Description</label>
+                                <textarea class="form-control ps-0 form-control-line" placeholder="ex: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea."></textarea>
+                        </div>
+                        <div class="white-line mt-3 mb-3"></div>
+
+        
+                        <a class="btn btn-outline-danger tombol-full" href="">Submit</a>
+                        
+                        
+                    </div>
+            </div>
+        </div>        
+    </section>
+        
+    </div>
+</div>
+</body>
+</html>
---
+++ crud.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="newStyle.css">
+    <title>movie-crud</title>
+</head>
+<body>
+    <div class="desktop">
+        <nav>
+            <div class="logo">
+                HealingTime
+            </div>
+            <ul>
+                <li class="movie"><a href="">Movie</a></li>
+                <li class="series"><a href="">Series</a></li>
+                <li class="cartoon"><a href="">Cartoon</a></li>
+                <li class="anime"><a href="">Anime</a></li>
+            </ul>
+            <div class="search"></div>
+            <div class="notification"></div>
+        </nav>
+        <div class="side-user"></div>
+            <img class="profile-user" src="img/unsplash_Zz5LQe-VSMY.png" alt="">
+            <div class="username">Katty Perry</div>
+            <div class="user-gmail">kattyper2032@gmail.com</div>
+                <div class="user-line" style="left: 1107px;top: 133px;"></div>
+                    <div class="create-movie"></div>
+                        <a class="text-crud" style="left: 1188px;top: 166px;" href="create.php">Create Movie</a>
+                    <div class="read-movie"></div>
+                        <a class="text-crud" style="left: 1188px;top: 234px;" href="">Read Movie</a>
+                    <div class="update-movie"></div>
+                        <a class="text-crud" style="left: 1188px;top: 309px;" href="">Update Movie</a>
+                    <div class="delete-movie"></div>
+                        <a class="text-crud" style="left: 1188px;top: 379px;" href="">Delete Movie</a>
+                <div class="user-line" style="left: 1107px;top: 467px;"></div>
+        <div class="box-news">
+            <div class="text-news">Today's Booming Movie</div>
+            <div class="img-news"></div>
+            <div class="text-news-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.</div>
+        </div>
+        <div class="box-next"></div>
+        <div class="box-film gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div class="box-film-info">
+                <div class="film"></div>
+                <div class="text-film">Now You See Me 2</div>
+                <img src="img/rate.svg" alt="">
+            </div>
+            <div class="box-film-info">
+                <div class="film"></div>
+                <div class="text-film">Now You See Me 2</div>
+                <img src="img/rate.svg" alt="">
+            </div>
+            <div class="box-film-info">
+                <div class="film"></div>
+                <div class="text-film">Now You See Me 2</div>
+                <img src="img/rate.svg" alt="">
+            </div>
+            <div class="box-film-info">
+                <div class="film"></div>
+                <div class="text-film">Now You See Me 2</div>
+                <img src="img/rate.svg" alt="">
+            </div>
+            <div class="box-film-info">
+                <div class="film"></div>
+                <div class="text-film">Now You See Me 2</div>
+                <img src="img/rate.svg" alt="">
+            </div>
+            <div class="box-film-info">
+                <div class="film"></div>
+                <div class="text-film">Now You See Me 2</div>
+                <img src="img/rate.svg" alt="">
+            </div>
+        </div>
+        <div class="box-iklan">
+
+        </div>
+    </div>
+</body>
+</html>
